62991edd1e4e0e76faf6f5c1dc2bf7c9c2c169de
Antonis Christofides <anthony@itia.ntua.gr>
2016-07-06 12:32:54 +0300

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="textbisect",
-    version="DEV",
+    version="0.1.0",
     license="GPL3",
     description="Binary search in a sorted text file",
     author="Antonis Christofides",
